b43d84176127a27a78d2410d8c10d0a7e8cd01ba
Programming-School <mohamedehabam99@hotmail.com>
2022-06-16 20:19:08 +0200

    Add new color button event

--- index.js
+++ index.js
@@ -2,54 +2,99 @@ const color = document.getElementById("colorDisplay");
 const squares = document.querySelectorAll(".square");
 const btns = document.getElementById("reset");
 const correct_message = document.getElementById("message");
-const h1 = document.querySelector('h1');
-let rgb_color = [];
+const colorDisplay = document.getElementById("colorDisplay");
+const h1 = document.getElementById("h1");
+const hardBtn = document.getElementById("hardBtn");
+const easyBtn = document.getElementById("easyBtn");
+const container = document.getElementById("container");
+let num_of_squares = 6;
+let rgb_color = generateRandomColors(num_of_squares);;
 let random_index;
+let selected;
 
 // Guess The Color
 function color_generator() {
-  for (let i = 0; i < squares.length; i++) {
-    let red = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    rgb_color.push(`rgb(${red}, ${blue}, ${green})`);
+  let red = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${blue}, ${green})`;
+}
+
+function generateRandomColors(genColor) {
+  let arr = [];
+  for (let i = 0; i < genColor; i++) {
+    arr.push(color_generator());
   }
+  return arr;
 }
 
 color_generator();
+generateRandomColors(num_of_squares);
 
-// Guess the number function
-function guess() {
-  for (let i = 0; i < squares.length; i++) {
-    for (let i = 0; i < squares.length; i++) {
-      random_index = Math.floor(Math.random() * rgb_color.length);
-      console.log(random_index);
-    }
-    color.innerText = rgb_color[random_index];
-
-    squares[i].style.backgroundColor = rgb_color[random_index];
+function new_colors() {
+  color_generator();
+  rgb_color = generateRandomColors(num_of_squares);
+  for (let g = 0; g < num_of_squares; g++) {
+    random_index = Math.floor(Math.random() * rgb_color.length);
+    selected = rgb_color[random_index];
+  }
+  color.innerText = rgb_color[random_index];
+  correct_message.textContent = "";
+  btns.innerText = "New Colors!";
+  h1.style.backgroundColor = "steelblue";
+  for (let i = 0; i < num_of_squares; i++) {
+    console.log(squares[i], rgb_color[i]);
+    squares[i].style.display = "block";
+    squares[i].style.backgroundColor = rgb_color[i];
+  }
+  for (let i = 0; i < num_of_squares; i++) {
+    squares[i].addEventListener("click", function () {
+      let clickedColor = this.style.backgroundColor;
+      if (clickedColor === selected) {
+        for (let i = 0; i < num_of_squares; i++) {
+          squares[i].style.backgroundColor = rgb_color[random_index];
+        }
+        correct_message.innerText = "Correct!";
+        btns.innerText = "Play Again?";
+        h1.style.backgroundColor = rgb_color[random_index];
+      } else {
+        this.style.backgroundColor = "#232323";
+        correct_message.innerText = "Try Again";
+      }
+    });
   }
 }
 
-guess();
+new_colors();
 
-squares[random_index].addEventListener("click", () => {
-  if (rgb_color[random_index]) {
-    squares[0].style.background = rgb_color[random_index];
-    squares[1].style.background = rgb_color[random_index];
-    squares[2].style.background = rgb_color[random_index];
-    squares[3].style.background = rgb_color[random_index];
-    squares[4].style.background = rgb_color[random_index];
-    squares[5].style.background = rgb_color[random_index];
-    correct_message.innerText = "Correct!";
-    btns.innerText = "Play Again?";
-    btns.addEventListener("click", () => {
-      btns.innerText = "New Colors!";
-      correct_message.innerText = "";
-      h1.style.backgroundColor = "steelblue";
-    });
-    h1.style.backgroundColor = rgb_color[random_index];
+btns.addEventListener("click", () => {
+  btns.innerText = "New Colors!";
+  correct_message.innerText = "";
+  h1.style.backgroundColor = "steelblue";
+  if (
+    container.innerHTML ===
+    "<div class='square'></div><div class='square'></div><div class='square'></div>"
+  ) {
+    new_colors();
   } else {
-    squares[random_index].style.display = "none";
+    new_colors();
   }
+});
+
+easyBtn.addEventListener("click", () => {
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  num_of_squares = 3;
+  container.innerHTML =
+    "<div class='square'></div><div class='square'></div><div class='square'></div>";
+  new_colors();
+});
+
+hardBtn.addEventListener("click", () => {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  num_of_squares = 6;
+  container.innerHTML =
+    "<div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div><div class='square'></div>";
+  new_colors();
 });
